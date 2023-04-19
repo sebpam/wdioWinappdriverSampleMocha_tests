e21c586f1e056ec2bc5cf88e4d9cc7504f14a929
sebpam <pambu76@hotmail.com>
2023-04-19 08:22:43 -0400

    readme file udpate

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# wdioWinappdriverSampleMocha_tests
+# Wdio Winappdriver Sample Mocha tests
+
+# Automated Tests Instructions
+
+## Installation
+### Prerequisites:
+
+- Installation of Nodes js https://nodejs.org/en/
+- Installation of VScode https://code.visualstudio.com/ or Git https://git-scm.com/downloads
+
+### Steps:
+- Clone repository from https://github.com/sebpam/wdioWinappdriverSampleMochatests.git
+- Run the following commands sequentially:
+
+```
+npm install
+```
+
+## Test run
+### Steps:
+
+From the root directory:
+```
+npx wdio run ./wdio.conf.js
+```
--- test/pages/calculatorFace.js
+++ test/pages/calculatorFace.js
@@ -1,29 +1,22 @@
 class CalculatorFace {
-    
-    numberKey ( number ) {
-        return $(`//*[@AutomationId=\"num${number}Button\"]`)
-    }
-
-    get plusSignKey () {
-        return $("//*[@AutomationId='plusButton']");
-    }
-
-    get equalSignKey () {
-        return $(`//*[@AutomationId=\"equalButton\"]`);
-    }
-
-    get calculationResult (){
-        return $("//*[@AutomationId='CalculatorResults']");
-    }
-
-    get clearKey (){
-        return $("//*[@AutomationId='clearButton']");
-    }
-
-    get clearMemomery(){
-        return $("//*[@AutomationId='MemPlus']");
-    }
-    
+  numberKey(number) {
+    return $(`//*[@AutomationId=\"num${number}Button\"]`);
+  }
+  get plusSignKey() {
+    return $("//*[@AutomationId='plusButton']");
+  }
+  get equalSignKey() {
+    return $(`//*[@AutomationId=\"equalButton\"]`);
+  }
+  get calculationResult() {
+    return $("//*[@AutomationId='CalculatorResults']");
+  }
+  get clearKey() {
+    return $("//*[@AutomationId='clearButton']");
+  }
+  get clearMemomery() {
+    return $("//*[@AutomationId='MemPlus']");
+  }
 }
 
-export default CalculatorFace
+export default CalculatorFace;
